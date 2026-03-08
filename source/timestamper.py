5f2e9b7f23073544c7ce8a609fb001c7d53e8d8c
jscodez <jason20@umbc.edu>
2026-03-07 20:51:59 -0500

    corrected window icon to display correctly.

--- source/timestamper.py
+++ source/timestamper.py
@@ -20,6 +20,9 @@ class TimeStamper:
     def __init__(self, root):
         self.root = root
         self.root.title("Time Stamper")
+
+        root.iconbitmap("icon.ico")
+
         self.root.geometry("450x600")
 
         self.timestamps = []
